abba4d0454fd862c665722f0a5c16933294e157c
Coeat.k <coeat.k@kakaocorp.com>
2021-05-21 16:05:04 +0900

    feat: changing build.gradle

--- src/main/java/com/theceres/webfluxtest/controller/TestController.java
+++ src/main/java/com/theceres/webfluxtest/controller/TestController.java
@@ -1,6 +1,24 @@
 package com.theceres.webfluxtest.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Slf4j
+@RestController
 public class TestController {
 
-
+    @GetMapping("/rtb")
+    public Mono<String> rtb(
+            ServerHttpRequest httpRequest) {
+        log.info("!!!!");
+        log.info(httpRequest.getRemoteAddress().toString());
+        Thread.currentThread().getThreadGroup().list();
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        return null;
+    }
 }
